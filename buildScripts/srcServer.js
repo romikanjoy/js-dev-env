a040530dc798cc79c36a99da9b70f4674ff29673
Romik Guha Anjoy <romik.03305@gmail.com>
2018-02-04 16:42:45 +0530

    Added Linting using ESLint

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -4,13 +4,15 @@ import open from 'open';
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
 
+/* eslint-disable no-console*/
+
 const port = 3000;
 const app = express();
 const compiler = webpack(config);
 
 app.use(require('webpack-dev-middleware')(compiler, {
-  noInfo: true,
-  publicPath: config.output.publicPath
+  publicPath: config.output.publicPath,
+  stats: 'minimal'
 }));
 
 app.get('/', (req, res) => {
